911c894af6927e2055963c0dda9f7141dd83f7c6
durden <63181030+durdenweb3@users.noreply.github.com>
2023-11-02 18:59:17 +0600

    Release

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('./db');
+const authRouter = require('./authRouter.js')
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use('/auth', authRouter);
+
 const start = async () => {
     try {
-        await sequelize.authenticate();
-        await sequelize.sync();
+        await mongoose.connect(process.env.DB_CONNECTION);
+
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
         });
